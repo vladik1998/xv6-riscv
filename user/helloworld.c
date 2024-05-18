23176dd1997e052e7f8e19dfe51662558e1eea4e
danielohbgu <danielohayon2019@gmail.com>
2024-05-18 09:32:58 +0000

    Assignment 1 - Task 1

---
+++ user/helloworld.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main()
+{
+    const char *s = "Hello World xv6\n";
+    write(1, s, strlen(s));
+    exit(0);
+}

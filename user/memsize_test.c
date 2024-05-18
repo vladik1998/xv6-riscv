6a6159ee60a98aa1d8fd624d2b9bec38836cbb96
danielohbgu <danielohayon2019@gmail.com>
2024-05-18 11:57:15 +0000

    Assignment 1 - Task 2

---
+++ user/memsize_test.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main()
+{
+  printf("This process is using %d bytes\n", memsize());
+  printf("Allocating 20,000 bytes using malloc...\n");
+  void *allocated = malloc(20000);
+  printf("This process is using %d bytes\n", memsize());
+  printf("Freeing 20,000 bytes using free...\n");
+  free(allocated);
+  printf("This process is using %d bytes\n", memsize());
+  exit(0);
+}
